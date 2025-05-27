f3224e94c9f23c81a51ece8b4de8b96f9154a764
janezkoprivec <koprivec.janez@gmail.com>
2025-05-27 10:50:02 +0200

    Centered the sculptures in the gallery view

--- components/sculpture-gallery.tsx
+++ components/sculpture-gallery.tsx
@@ -242,7 +242,7 @@ export default function SculptureGallery() {
 
   return (
     <div className="container mx-auto px-4 py-8">
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+      <div className="flex flex-row justify-center gap-8">
         {sculptures.map((sculpture) => (
           <SculptureCard
             key={sculpture.id}
